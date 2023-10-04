213e73ae363a16c139cacb04f8ad44cc0b94a6ff
Vikkans <Jakobsenviktor6@gmail.com>
2023-10-04 10:34:59 +0200

    Add project files.

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp23
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? Svara j för ja och n för nej");
+            string svar = Console.ReadLine();
+            
+
+            Console.WriteLine("Hur gammal är du?");
+            string svar2 = Console.ReadLine();
+            int ålder = int.Parse(svar2);
+
+            if (ålder < 22 && svar == "j") 
+            {
+                Console.WriteLine("Vi vill gärna anställa dig.");
+            }
+            else { Console.WriteLine("Vi letar tyvär efter någon annan."); }
+            Console.ReadKey();
+        }
+    }
+}
